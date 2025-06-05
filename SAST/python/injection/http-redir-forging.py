b7058b4aba3e3205a4a33a06a92a186925cf3720
danilofantinato <danilofantinato@gmail.com>
2025-06-05 00:10:59 -0400

    Security remediation for SAST/python/injection/http-redir-forging.py

--- SAST/python/injection/http-redir-forging.py
+++ SAST/python/injection/http-redir-forging.py
@@ -1,15 +1,17 @@
-# The following noncompliant code example is vulnerable 
-# to open redirection as it constructs a URL with 
-# user-controllable data. This URL is then used to 
-# redirect the user without being first validated. An 
-# attacker can leverage this to manipulate users into 
-# performing unwanted redirects.
-
-from flask import Flask, redirect
+from flask import Flask, redirect, request
+from urllib.parse import urlparse, urljoin
 
 app = Flask("example")
 
 @app.route("/redirect")
 def redirect():
-    url = request.args["url"]
-    return redirect(url) # Noncompliant
+    url = request.args.get("url")
+    if url:
+        parsed_url = urlparse(url)
+        allowed_hosts = ["example.com", "www.example.com"]
+        if parsed_url.netloc in allowed_hosts:
+            return redirect(url)
+    return redirect("/")
+
+if __name__ == "__main__":
+    app.run()
