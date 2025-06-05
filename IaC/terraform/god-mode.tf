6891ac80c7e24d8f7563bf8b25ecff6152ba2084
danilofantinato <danilofantinato@gmail.com>
2025-06-05 00:10:29 -0400

    Security remediation for IaC/terraform/god-mode.tf

--- IaC/terraform/god-mode.tf
+++ IaC/terraform/god-mode.tf
@@ -1,26 +1,20 @@
-# A policy that grants all permissions may indicate an improper access control, which violates the principle of least privilege.
-
-#     OWASP Top 10 2021 Category A1 - Broken Access Control
-#     AWS Documentation - Grant least privilege
-#     Google Cloud Documentation - Understanding roles
-#     MITRE, CWE-732 - Incorrect Permission Assignment for Critical Resource
-#     MITRE, CWE-284 - Improper Access Control
-#     OWASP Top 10 2017 Category A5 - Broken Access Control
-
 # AWS
 resource "aws_iam_policy" "example" {
-  name = "noncompliantpolicy"
+  name = "least_privilege_policy"
 
   policy = jsonencode({
-    Version   = "2012-10-17"
+    Version = "2012-10-17"
     Statement = [
       {
-        Action   = [
-          "*" # Sensitive
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject"
         ]
         Effect   = "Allow"
         Resource = [
-          aws_s3_bucket.mybucket.arn
+          aws_s3_bucket.mybucket.arn,
+          "${aws_s3_bucket.mybucket.arn}/*"
         ]
       }
     ]
@@ -30,7 +24,7 @@ resource "aws_iam_policy" "example" {
 # GCP
 resource "google_project_iam_binding" "example" {
   project = "example"
-  role    = "roles/owner" # Sensitive
+  role    = "roles/viewer"
 
   members = [
     "user:jane@example.com",
