2e62ead75ff6130e1531b23138b73ff8897e578f
danilofantinato <danilofantinato@gmail.com>
2025-06-05 00:11:03 -0400

    Security remediation for SAST/python/ciphers/pycrypto.py

--- SAST/python/ciphers/pycrypto.py
+++ SAST/python/ciphers/pycrypto.py
@@ -1,20 +1,21 @@
-# Strong cipher algorithms are cryptographic systems resistant to 
-# cryptanalysis, they are not vulnerable to well-known attacks like 
-# brute force attacks for example.
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
 
-# It’s not recommended to use algorithm with a block size inferior 
-# than 128 bits.
+# AES-256 in GCM mode
+key = b'ThisIsAVeryLongKeyForAES256Cipher'
+iv = b'ThisIsAVeryLongIV'
 
-from Crypto.Cipher import *
+cipher = Cipher(algorithms.AES(key), modes.GCM(iv), backend=default_backend())
+encryptor = cipher.encryptor()
+ciphertext = encryptor.update(b"This is a secret message") + encryptor.finalize()
 
-des3 = DES.new('ChangeIt') # Noncompliant: DES works with 56-bit keys allow attacks via exhaustive search
-tdes3 = DES3.new('ChangeItChangeIt') # Noncompliant: Triple DES is vulnerable to meet-in-the-middle attack
-bf2 = Blowfish.new('ChangeItWithYourKey', Blowfish.MODE_CBC, 'ChangeIt') # Noncompliant: Blowfish use a 64-bit block size makes it
-rc21 = ARC2.new('ChangeItWithYourKey', ARC2.MODE_CFB, 'ChangeIt') # Noncompliant: RC2 is vulnerable to a related-key attack
-rc41 = ARC4.new('ChangeItWithYourKey') # Noncompliant: vulnerable to several attacks (see https://en.wikipedia.org/wiki/RC4#Security)
+# ChaCha20-Poly1305
+key = b'ThisIsAVeryLongKeyForChaCha20Poly1305'
+iv = b'ThisIsAVeryLongIV'
 
+cipher = Cipher(algorithms.ChaCha20(key, iv), mode=None, backend=default_backend())
+encryptor = cipher.encryptor()
+ciphertext = encryptor.update(b"This is a secret message") + encryptor.finalize()
 
-    # OWASP Top 10 2021 Category A2 - Cryptographic Failures
-    # OWASP Top 10 2017 Category A6 - Security Misconfiguration
-    # MITRE, CWE-327 - Use of a Broken or Risky Cryptographic Algorithm
-    # SANS Top 25 - Porous Defenses
+# Authenticated encryption should be used whenever possible
+# to protect against chosen-ciphertext attacks and other vulnerabilities.
