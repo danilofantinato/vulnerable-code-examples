5b9983df28938ac21b07e4fd1068f42bf2fbce33
danilofantinato <danilofantinato@gmail.com>
2025-06-05 00:11:13 -0400

    Security remediation for SAST/javascript/sample.js

--- SAST/javascript/sample.js
+++ SAST/javascript/sample.js
@@ -1,10 +1,7 @@
-function insecureFunction(password) {
-    console.log("Received password: " + password);
+function secureFunction(password) {
+    // Implement secure password handling logic here
+    // For example, hash the password before storing or transmitting
   }
   
   var userInput = "sensitivePassword";
-  insecureFunction(userInput);
-
-// (CWE-359)
-// This sample JavaScript file contains an insecure function that logs a password to the console. 
-// It can be used to test SAST tools' ability to identify sensitive information exposure.
+  secureFunction(userInput);
