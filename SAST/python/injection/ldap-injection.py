cb1cd6435eb7073b9df0df6517576e86104c6110
danilofantinato <danilofantinato@gmail.com>
2025-06-05 00:10:57 -0400

    Security remediation for SAST/python/injection/ldap-injection.py

--- SAST/python/injection/ldap-injection.py
+++ SAST/python/injection/ldap-injection.py
@@ -1,18 +1,24 @@
-# The following noncompliant code is vulnerable 
-# to LDAP injection because untrusted data is 
-# concatenated to an LDAP query without prior 
-# sanitization or validation.
+# Remediated code:
 
 from flask import request
 import ldap
+from ldap import filter
 
 @app.route("/user")
 def user():
-    username =  request.args['username']
+    username = request.args.get('username')
 
-    search_filter = "(&(objectClass=user)(uid="+username+"))"
+    if not username:
+        return "Username is required", 400
+
+    # Sanitize the input using the ldap.filter.escape_filter_chars function
+    sanitized_username = ldap.filter.escape_filter_chars(username)
+
+    search_filter = "(&(objectClass=user)(uid=" + sanitized_username + "))"
 
     ldap_connection = ldap.initialize("ldap://localhost:389")
-    user = ldap_connection.search_s("dc=example,dc=org", ldap.SCOPE_SUBTREE, search_filter) # Noncompliant
-
-    return user[0]
+    try:
+        user = ldap_connection.search_s("dc=example,dc=org", ldap.SCOPE_SUBTREE, search_filter)
+        return user[0]
+    except ldap.LDAPError as e:
+        return f"LDAP error: {str(e)}", 500
