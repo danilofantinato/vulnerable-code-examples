56cdbc34627ccf645ab12c05fa9389c57e1b9c82
danilofantinato <danilofantinato@gmail.com>
2025-06-05 00:10:26 -0400

    Security remediation for IaC/terraform/public-api.tf

--- IaC/terraform/public-api.tf
+++ IaC/terraform/public-api.tf
@@ -1,11 +1,44 @@
-# A public API, which can be requested by any authenticated or unauthenticated identities, can lead to unauthorized actions and information disclosures.
-
-    # OWASP Top 10 2021 Category A1 - Broken Access Control
-    # AWS Documentation - Controlling and managing access to a REST API in API Gateway
-    # MITRE, CWE-284 - Improper Access Control
-    # OWASP Top 10 2017 Category A5 - Broken Access Control
-
-resource "aws_api_gateway_method" "noncompliantapi" {
-  authorization = "NONE" # Sensitive
+resource "aws_api_gateway_method" "compliantapi" {
+  authorization = "AWS_IAM"
   http_method   = "GET"
+
+  request_models = {
+    "application/json" = aws_api_gateway_model.request_model.name
+  }
+
+  request_validator_id = aws_api_gateway_request_validator.request_validator.id
+}
+
+resource "aws_api_gateway_model" "request_model" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  name        = "RequestModel"
+  description = "Request model for the API"
+  content_type = "application/json"
+
+  schema = <<EOF
+{
+  "$schema": "http://json-schema.org/draft-04/schema#",
+  "title": "RequestModel",
+  "type": "object",
+  "properties": {
+    "userId": {
+      "type": "string"
+    },
+    "requestData": {
+      "type": "object"
+    }
+  },
+  "required": [
+    "userId",
+    "requestData"
+  ]
+}
+EOF
+}
+
+resource "aws_api_gateway_request_validator" "request_validator" {
+  rest_api_id                 = aws_api_gateway_rest_api.api.id
+  name                        = "RequestValidator"
+  validate_request_body       = true
+  validate_request_parameters = true
 }
